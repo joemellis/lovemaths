be678b0dfd1888b90ad99f90d75f34279ebae704
joemellis <joemellis1983@gmail.com>
2024-01-19 20:50:59 +0000

    add code to calculate 2 random numbers for te question

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ You can safely delete this README.md file, or change it for your own project. Pl
 ## Codeanywhere Reminders
 
 To run a frontend (HTML, CSS, Javascript only) application in Codeanywhere, in the terminal, type:
-
-`python3 -m http.server`
+python3 -m http.server
+``
 
 A button should appear to click: _Open Preview_ or _Open Browser_.
 
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,51 +1,58 @@
-// wait for the DOM to finish loading before running the game
-// Grt thr button elements and add event listeners 
-document.addEventListener("DOMContentLoaded",function(){
-    let buttons= document.getElementsByTagName("button");
-    for(let button of buttons){
-        button.addEventListener(click,function(){
-            if (this,this.getAttribute("data-type") === "submit"){
-                alert("you clicked submit")
-            } else{
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`you clicked ${gameType}`)
+                alert(`You clicked ${gameType}`);
             }
-        })
+        });
     }
-})
 
-function runGame(){
+});
 
-}
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame() {
 
-function checkAnswer(){
-
-}
-
-function calculateCorrectAnswer(){
-
-}
-
-function incrementScore(){
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
 }
 
-function incrementScore(){
+function checkAnswer() {
 
 }
 
-function incrementWrongAnswer(){
+function calculateCorrectAnswer() {
 
 }
 
-function displayAdditionQuestion(){
+function incrementScore() {
 
 }
 
-function displaySubtractionQuestion(){
+function incrementWrongAnswer() {
 
 }
 
-function displayMultiplyQuestion(){
+function displayAdditionQuestion(operand1, operand2) {
 
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
 }
